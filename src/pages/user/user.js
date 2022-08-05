a70694ce406c8edcbd3d4fa97e08c42f22e972f5
Cibely Pereira <cibely.port@gmail.com>
2022-08-05 11:02:25 -0300

    alterações

--- src/pages/user/user.js
+++ src/pages/user/user.js
@@ -2,17 +2,15 @@ import { Input } from '../../components/input'
 import { Button } from '../../components/button'
 
 export const User = () => {
-  const handlerClick = () => {
-    alert('Salvo')
-  }
+  const userEmail = (email) => console.log(email)
 
   return (
     <>
       <form>
-        <Input label="Login" type="text" placeholder='Escreva seu nome' /> <br />
+        <Input label="Login" type="email" placeholder='Digite seu login' /> <br />
         <Input label="Password" type="text" placeholder='Digite sua senha' />  <br />
         <a href="#">Esqueceu sua senha</a> <br />
-        <Button onClick={handlerClick} type='button'>Entrar</Button> <br />
+        <Button onClick={userEmail} type='button'>Entrar</Button> <br />
       </form>
     </>
   )
