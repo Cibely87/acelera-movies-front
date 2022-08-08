12afdb26b14008048d81632cc97a79cdf847c823
Cibely Pereira <cibely.port@gmail.com>
2022-08-08 11:09:41 -0300

    alterações login e home

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import { Routes, Route, BrowserRouter } from 'react-router-dom'
 import { User } from './pages/user/user'
+import { Home } from './pages/home/index'
 
 export const App = () => {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<User />} exact />
+        <Route path="/home" element={<Home />} exact />
       </Routes>
     </BrowserRouter>
   )
---
+++ src/pages/home/index.js
@@ -0,0 +1,10 @@
+import './home.css'
+// import { Input } from '../../components/input'
+
+export const Home = () => {
+  return (
+    <div className='homePage'>
+      <h1>All Movies</h1>
+    </div>
+  )
+}
