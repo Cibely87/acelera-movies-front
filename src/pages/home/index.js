82d207fa6124079b023c8b63a6bed77976868daa
Cibely Pereira <cibely.port@gmail.com>
2022-12-22 22:24:30 -0300

    mudancas layout

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -29,7 +29,7 @@ export const Home = () => {
         <aside>
           <Modal
             label="New Movie"
-            title="A new movie">
+            title="Movie">
             <Formulario />
           </Modal>
         </aside>
